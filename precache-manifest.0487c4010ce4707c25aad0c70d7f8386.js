c73115e472945677e63166462fd7850cbbc6fb68
sanketkumar <sanket.kr7922@gmail.com>
2019-08-15 11:46:09 +0530

    Updates

---
+++ precache-manifest.0487c4010ce4707c25aad0c70d7f8386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3f947c74977248999aba4b3ba1c236",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "664249185c4b60f3f102",
+    "url": "/dad-jokes/static/css/main.de1a89d3.chunk.css"
+  },
+  {
+    "revision": "b942735f43fb0347b3cd",
+    "url": "/dad-jokes/static/js/2.66f03414.chunk.js"
+  },
+  {
+    "revision": "664249185c4b60f3f102",
+    "url": "/dad-jokes/static/js/main.1324820e.chunk.js"
+  },
+  {
+    "revision": "3cccbffd9a908bf7dbad",
+    "url": "/dad-jokes/static/js/runtime~main.d0c0f51b.js"
+  },
+  {
+    "revision": "9f6dc92a4dd9c3517787deefff688e29",
+    "url": "/dad-jokes/static/media/happy.9f6dc92a.png"
+  }
+]);
